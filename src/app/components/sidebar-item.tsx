7873270521822c136eabc96c85884a320b57e56e
JuliaPriadka <priadkajulia@gmail.com>
2026-05-08 16:53:25 +0300

    Added Link to Sidebar-item and navigation to Sidebar

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import clsx from "clsx";
 
 export interface SidebarItemProps {
+    active?:boolean;
 href:string;
 src:string;
 alt:string;
 children:React.ReactNode;
 }
 
-export default function SidebarItem({href,src,alt,children}:SidebarItemProps) {
+export default function SidebarItem({href,src,alt,children,active}:SidebarItemProps) {
     return (
         <li>
-        <a href={href} className="flex items-center gap-3.5">
+        <Link href={href} className={clsx("flex items-center gap-3.5 mx-1 h-6 after:ml-auto",active&& "after:w-1 after:h-full after:bg-purple-400 after:rounded-2xl")}>
 <Image src={src} alt={alt} width={18} height={18}/>
 <span className="font-medium text-base text-neutral-50">{children}</span>
-        </a>
+        </Link>
         </li>
     )
 };
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,21 +1,31 @@
+"use client"
+
 import React from "react";
 import Image from "next/image";
 import SidebarItem from "./sidebar-item";
+import { usePathname, useRouter } from "next/navigation";
+
 
 export interface SidebarProps{};
 
 
 export default function Sidebar({}:SidebarProps) {
+    const pathname=usePathname();
+    const router=useRouter();
+    const onExitClick=()=>{
+        router.push("/");
+    };
+
     return(
         <aside className="fixed top-0 left-0 w-60 h-screen">
-<div className="bg-gray-900 flex flex-col h-full overflow-y-auto pt-8 pb-10 pl-4">
+<div className="bg-gray-900 flex flex-col h-full overflow-y-auto pt-8 pb-10">
 <Image src="/icons/Frame 1000003593.png" width={122} height={24 }alt="logo" className="mb-20 mx-auto"/>
 <ul className="flex flex-col gap-7">
-<SidebarItem href="/dashboard" src="/icons/squares-2x2.png" alt="squares picture">Dashboard</SidebarItem>
-<SidebarItem href="/companies" src="/icons/briefcase.png" alt="briefcase picture">Companies</SidebarItem>
+<SidebarItem href="/dashboard" src="/icons/squares-2x2.png" alt="squares picture" active={pathname==="/dashboard"}>Dashboard</SidebarItem>
+<SidebarItem href="/companies" src="/icons/briefcase.png" alt="briefcase picture" active={pathname==="/companies"}>Companies</SidebarItem>
 </ul>
-<button type="button" className="flex items-center gap-2 mt-auto mx-auto">
-<Image src="/icons/arrow-left-on-rectangle.png" alt="exit" width={18} height={18}/>
+<button type="button" className="flex items-center gap-2 mt-auto mx-auto" onClick={onExitClick}>
+<Image src="/icons/arrow-left-on-rectangle.png" alt="exit" width={18} height={18} />
 <span className="font-medium text-neutral-50 text-base">Exit</span>
 </button>
 </div>
